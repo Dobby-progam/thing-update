1c254aca7b88b7686ccb3f9764f2f4168982714b
MiataBoy <miataboymx@gmail.com>
2023-03-31 17:26:01 +0200

    Fix some things
    Signed-off-by: MiataBoy <miataboymx@gmail.com>

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
 
 <h2>Registration</h2>
 
-<form action="/authentication.php" method="post">
+<form action="/authentication.php" method="post" name="registration">
     <div class="imgcontainer">
         <img src="assets/images/BlueAtomic/AtomicLogo.png" alt="Logo">
     </div>
@@ -21,10 +21,10 @@
         <label for="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
 
-        <button type="submit" value="register">Register</button>
+        <button type="submit" name="registration">Register</button>
 
         <label>
-            <input type="checkbox" name="remember" required> I agree to the privacy policy.
+            <input type="checkbox" name="privacy policy" required> I agree to the privacy policy.
         </label>
     </div>
 
