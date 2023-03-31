edb75cb8f83c52fc202162ca5651657db5653f89
MiataBoy <miataboymx@gmail.com>
2023-03-31 17:25:27 +0200

    in progress rework of authentication script
    Signed-off-by: MiataBoy <miataboymx@gmail.com>

--- authentication.php
+++ authentication.php
@@ -1,32 +1,69 @@
 <?php
 
-$env = parse_ini_file('.env');
+function dbCommand(string $query, array $parameters) : mixed {
+    $env = parse_ini_file('.env');
 
-$user = $env['USER'];
-$password = $env['PASSWORD'];
+    $user = $env['USER'];
+    $password = $env['PASSWORD'];
 
-$conn = new PDO('mysql:host=localhost;dbname=atomicforum', $user, $password);
-//$conn = new mysqli($host, $user, $password, $database) or die('could not connect');
+    $conn = new PDO('mysql:host=localhost;dbname=atomicforum', $user, $password);
 
-$stmt = $conn->prepare("SELECT password FROM auth WHERE username = ?");
-$stmt->execute([$_POST["uname"]]);
+    $stmt = $conn->prepare($query);
+    $stmt->execute($parameters);
+    return $stmt->fetch();
+}
 
-$result = $stmt->fetch();
-//var_dump($row);
-
-if (empty($result)) {
-    $password = password_hash($_POST["psw"], PASSWORD_DEFAULT);
-
-    $username = $_POST['uname'];
-    $query = $conn->prepare("INSERT INTO auth (username, password) VALUES (?, ?)");
-    $query->execute([$_POST["uname"], $password]);
-} else {
-    if (password_verify($_POST['psw'], $result['password'])) {
-        echo 'password correct';
+function verifyPassword(string $password) : bool {
+    if (password_verify($_POST['psw'], $password)) {
+        return true;
     } else {
-        session_start();
-        $_SESSION['badlogin'] = "password";
-        header("Location: login.php");
-        exit();
+        return false;
     }
 }
+
+if (isset($_POST['registration'])) {
+    echo 'new';
+} elseif (isset($_POST['login'])) {
+    $username = dbCommand('SELECT username FROM auth WHERE username = ?', [$_POST['uname']]);
+    var_dump($username);
+
+    if (empty($username)) {
+        session_start();
+        $_SESSION['badlogin'] = 404;
+        header("location: login.php");
+        exit();
+    } else {
+
+        $userPassword = dbCommand('SELECT password FROM auth WHERE username = ?', [$_POST['uname']]);
+        $correctPassword = verifyPassword($userPassword['password']);
+
+        if ($correctPassword) {
+            echo 'password correct';
+        } else {
+            echo 'password bad';
+            session_start();
+            $_SESSION['badlogin'] = 403;
+            header("Location: login.php");
+            exit();
+        }
+    }
+
+
+}
+//
+//if (empty($result)) {
+//    $password = password_hash($_POST["psw"], PASSWORD_DEFAULT);
+//
+//    $username = $_POST['uname'];
+//    $query = $conn->prepare("INSERT INTO auth (username, password) VALUES (?, ?)");
+//    $query->execute([$_POST["uname"], $password]);
+//} else {
+//    if (password_verify($_POST['psw'], $result['password'])) {
+//        echo 'password correct';
+//    } else {
+//        session_start();
+//        $_SESSION['badlogin'] = "password";
+//        header("Location: login.php");
+//        exit();
+//    }
+//}
