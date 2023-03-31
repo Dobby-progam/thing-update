9afb81a621f55b07e0a8e4bea614566fbe03fceb
MiataBoy <miataboymx@gmail.com>
2023-03-31 19:09:30 +0200

    Filter for already existent usernames
    Signed-off-by: MiataBoy <miataboymx@gmail.com>

--- authentication.php
+++ authentication.php
@@ -22,7 +22,16 @@ function verifyPassword(string $password) : bool {
 }
 
 if (isset($_POST['registration'])) {
-    echo 'new';
+    if (!empty(dbCommand('SELECT username FROM auth WHERE username = ?', [$_POST['uname']]))) {
+        session_start();
+        $_SESSION['badlogin'] = 403;
+        header("Location: register.php");
+        exit();
+    }
+
+    $passwordHash = password_hash($_POST["psw"], PASSWORD_DEFAULT);
+    $handled = dbCommand('INSERT INTO auth (username, password) VALUES (?, ?)', [$_POST['uname'], $passwordHash]);
+    echo $handled;
 } elseif (isset($_POST['login'])) {
     $username = dbCommand('SELECT username FROM auth WHERE username = ?', [$_POST['uname']]);
     var_dump($username);
@@ -47,23 +56,4 @@ if (isset($_POST['registration'])) {
             exit();
         }
     }
-
-
 }
-//
-//if (empty($result)) {
-//    $password = password_hash($_POST["psw"], PASSWORD_DEFAULT);
-//
-//    $username = $_POST['uname'];
-//    $query = $conn->prepare("INSERT INTO auth (username, password) VALUES (?, ?)");
-//    $query->execute([$_POST["uname"], $password]);
-//} else {
-//    if (password_verify($_POST['psw'], $result['password'])) {
-//        echo 'password correct';
-//    } else {
-//        session_start();
-//        $_SESSION['badlogin'] = "password";
-//        header("Location: login.php");
-//        exit();
-//    }
-//}
--- register.php
+++ register.php
@@ -15,11 +15,19 @@
     </div>
 
     <div class="container">
+
         <label for="uname"><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
 
         <label for="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+        <?php
+        session_start();
+        if (isset($_SESSION['badlogin']) and $_SESSION["badlogin"] == 403) :?>
+            <span style="color: red">A user with this name already exists.</span>
+        <?php endif;
+        session_destroy();?>
 
         <button type="submit" name="registration">Register</button>
 
