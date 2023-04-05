43e3dbcfdca763175caa19fc696474771ed1e0fe
MiataBoy <miataboymx@gmail.com>
2023-04-05 14:43:22 +0200

    add e-mail intake
    Signed-off-by: MiataBoy <miataboymx@gmail.com>

--- login.php
+++ login.php
@@ -15,17 +15,17 @@
     </div>
 
     <div class="container">
-        <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
+        <label for="user"><b>Username or e-mail address</b></label>
+        <input type="text" placeholder="Enter Username" name="user" id="user" required>
 
         <label for="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
 
         <?php
         session_start();
-        if (isset($_SESSION['badlogin']) and $_SESSION["badlogin"] == 404) :?>
-            <span style="color: yellow">This user does not exist.</span>
-        <?php elseif (isset($_SESSION['badlogin']) and $_SESSION["badlogin"] == 403) :?>
+        if (isset($_SESSION['badAuth']) and $_SESSION["badAuth"] == 404) :?>
+            <span style="color: red">Username or e-mail address does not exist. <a href="register.php">Try registering</a>.</span>
+        <?php elseif (isset($_SESSION['badAuth']) and $_SESSION["badAuth"] == 403) :?>
             <span style="color: red">Password incorrect.</span>
         <?php endif;
         session_destroy()?>
