b853969462df69a4ff49012521999264013de0ce
MiataBoy <miataboymx@gmail.com>
2023-04-05 14:42:54 +0200

    Add user ID primary key, e-mail input
    Signed-off-by: MiataBoy <miataboymx@gmail.com>

--- authentication.php
+++ authentication.php
@@ -1,7 +1,4 @@
 <?php
-// TODO: Add mail addresses
-// TODO: Add user IDs (auto increment)
-// TODO: Make user IDs primary key
 function dbCommand(string $query, array $parameters) : mixed {
     $env = parse_ini_file('.env');
 
@@ -10,9 +7,13 @@ function dbCommand(string $query, array $parameters) : mixed {
 
     $conn = new PDO('mysql:host=localhost;dbname=atomicforum', $user, $password);
 
-    $stmt = $conn->prepare($query);
-    $stmt->execute($parameters);
-    return $stmt->fetch();
+    try {
+        $stmt = $conn->prepare($query);
+        $stmt->execute($parameters);
+        return $stmt->fetch();
+    } catch (PDOException $error) {
+        return [$error->getCode(), $error];
+    }
 }
 
 function verifyPassword(string $password) : bool {
@@ -23,37 +24,60 @@ function verifyPassword(string $password) : bool {
     }
 }
 
+function isMail(string $mail) : bool {
+    $mail = filter_var($mail, FILTER_SANITIZE_EMAIL);
+
+    if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 if (isset($_POST['registration'])) {
-    if (!empty(dbCommand('SELECT username FROM auth WHERE username = ?', [$_POST['uname']]))) {
+    if (!isMail($_POST['mail'])) {
         session_start();
-        $_SESSION['badlogin'] = 403;
+        $_SESSION['badAuth'] = 404;
         header("Location: register.php");
         exit();
     }
 
     $passwordHash = password_hash($_POST["psw"], PASSWORD_DEFAULT);
-    $handled = dbCommand('INSERT INTO auth (username, password) VALUES (?, ?)', [$_POST['uname'], $passwordHash]);
-    echo $handled;
-} elseif (isset($_POST['login'])) {
-    $username = dbCommand('SELECT username FROM auth WHERE username = ?', [$_POST['uname']]);
-    var_dump($username);
 
-    if (empty($username)) {
+    $regInput = dbCommand('INSERT INTO authentication (username, mail, password) VALUES (?, ?, ?)', [$_POST['uname'], $_POST['mail'], $passwordHash]);
+
+    if (!is_bool($regInput)) {
+        if ($regInput[0] == 23000) {
+            session_start();
+            $_SESSION['badAuth'] = 403;
+            header("Location: register.php");
+            exit();
+        } else {
+            echo $regInput[1];
+        }
+    }
+} elseif (isset($_POST['login'])) {
+    if (isMail($_POST['user'])) {
+        $user = dbCommand('SELECT * FROM authentication WHERE mail = ?', [$_POST['user']]);
+    } else {
+        $user = dbCommand('SELECT * FROM authentication WHERE username = ?', [$_POST['user']]);
+    }
+
+    if (empty($user)) {
         session_start();
-        $_SESSION['badlogin'] = 404;
+        $_SESSION['badAuth'] = 404;
         header("location: login.php");
         exit();
     } else {
 
-        $userPassword = dbCommand('SELECT password FROM auth WHERE username = ?', [$_POST['uname']]);
-        $correctPassword = verifyPassword($userPassword['password']);
+        $correctPassword = verifyPassword($user['password']);
 
         if ($correctPassword) {
             echo 'password correct';
         } else {
             echo 'password bad';
             session_start();
-            $_SESSION['badlogin'] = 403;
+            $_SESSION['badAuth'] = 403;
             header("Location: login.php");
             exit();
         }
