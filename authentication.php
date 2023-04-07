71eee3ab90bcfc0a6908209e7a7d7c807da50ab1
MiataBoy <miataboymx@gmail.com>
2023-04-07 14:05:11 +0200

    move functions
    Signed-off-by: MiataBoy <miataboymx@gmail.com>

--- authentication.php
+++ authentication.php
@@ -1,48 +1,5 @@
 <?php
-
-use JetBrains\PhpStorm\NoReturn;
-
-function dbCommand(string $query, array $parameters) : mixed {
-    $env = parse_ini_file('.env');
-
-    $user = $env['USER'];
-    $password = $env['PASSWORD'];
-
-    $conn = new PDO('mysql:host=localhost;dbname=atomicforum', $user, $password);
-
-    try {
-        $stmt = $conn->prepare($query);
-        $stmt->execute($parameters);
-        return $stmt->fetch();
-    } catch (PDOException $error) {
-        return [$error->getCode(), $error];
-    }
-}
-
-function verifyPassword(string $password) : bool {
-    if (password_verify($_POST['psw'], $password)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isMail(string $mail) : bool {
-    $mail = filter_var($mail, FILTER_SANITIZE_EMAIL);
-
-    if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-#[NoReturn] function redirect(string $page, int $code) : void {
-    session_start();
-    $_SESSION['badAuth'] = $code;
-    header("Location: $page");
-    exit();
-}
+require_once('scripts/php/functions.php');
 
 if (isset($_POST['registration'])) {
     if (!isMail($_POST['mail'])) {
